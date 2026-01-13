324458c1594f95074daa2016d32bc3c580fe757d
kavindasj-at-832266882009 <kavindasj@cf.lk>
2026-01-13 11:44:54 +0530

    SecondLargest

---
+++ src/leetcode/SecondLargest.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        int[] numbers={1,4,2,5,6};
+        secondLarge(numbers);
+    }
+    static void secondLarge(int[] numbers){
+        int first=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+
+        for(int num:numbers){
+            if(num>first){
+                second=first;
+                first=num;
+            } else if (num>second && num!=first){
+                second=num;
+            }
+        }
+        System.out.println("second large : " +second);
+
+
+    }
+}
