348ce279992719cdc199fb1cfb2c9738b271a6f4
kavindasj-at-832266882009 <kavindasj@cf.lk>
2026-01-13 15:10:21 +0530

    nonRepeatingElement

---
+++ src/leetcode/MissingNumber.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[]  numbers={1, 2, 4, 5};
+        missingNumber(numbers);
+
+
+    }
+    static void missingNumber(int[] numbers){
+        int expectedSum=0;
+        int actualSum=0;
+
+        for(int n:numbers){
+            actualSum+=n;
+        }
+        System.out.println("actualSum"+actualSum);
+        for (int n=1;n<=numbers.length+1;n++){
+            expectedSum+=n;
+        }
+        System.out.println("expectedSum"+expectedSum);
+
+        int missingNum=expectedSum-actualSum;
+        System.out.println(missingNum);
+
+    }
+}
+
