100dc15d5f2da8193dce88b245e48492855e8033
kavindasj-at-832266882009 <kavindasj@cf.lk>
2026-01-16 15:51:51 +0530

    Palindrome

---
+++ src/leetcode/Palindrome.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(palindrome("ABAT"));
+    }
+    static Boolean palindrome(String word){
+        int left=0;
+        char[] arr=word.toCharArray();
+        int right =arr.length-1;
+
+        while(left<right){
+            if(arr[right]!=arr[left]){
+                return false;
+            }
+            right--;
+            left++;
+        }
+        return true;
+    }
+}
